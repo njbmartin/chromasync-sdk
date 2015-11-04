ee0daaa381908e6bb0e551729a60da4a6de68969
Nico <n.martin@movura.co.uk>
2015-11-04 00:02:39 +0000

    Create CSGO_main.lua

---
+++ sample-lua-scripts/CSGO_main.lua
@@ -0,0 +1,47 @@
+-- All functions must sit within a unique class
+-- This prevents any conflicts with other lua scripts
+CSGO_Example = {}
+
+local colore = clr.Corale.Colore.Core
+local keyboard = colore.Keyboard.Instance
+local thread = clr.System.Threading.Thread
+
+-- CS:GO Specific fields
+
+local team = "NA"
+local _isAnimating = false
+local _phase = "NA"
+
+-- FreezeTime Animation
+CSGO_Example.FreezeTime = coroutine.create(function ()
+	while true do
+		_isAnimating = true
+		if phase ~= "freezetime" then
+		_isAnimating = false
+			coroutine.yield()			
+		end
+		keyboard.SetAll(colore.Color(255,0,0))
+		thread.Sleep(200)
+		keyboard.SetAll(colore.Color(0,0,255))
+		thread.Sleep(200)
+	end
+end)
+
+
+function CSGO_Example.PhaseHandler(phase)
+
+	if phase == "freezetime" then -- Check if Phase is FreezeTime
+		DebugLua("freezetime")
+		coroutine.resume(CSGO_Example.FreezeTime)
+	end
+end
+
+-- our main function to handle the data 
+function CSGO_Example.handleData(json)	
+	-- Get the current phase (if any)
+	phase = json["round"]["phase"]
+	CSGO_Example.PhaseHandler(phase)
+end
+
+-- Finally, we must register this script in order to receive data
+RegisterForEvents("Counter-Strike: Global Offensive", CSGO_Example.handleData)
