b8fe93bb857731076ec5a4f1e6b5375bd940288b
Nico <n.martin@movura.co.uk>
2015-11-13 11:56:38 +0000

    Update CSGO_main.lua

--- sample-lua-scripts/CSGO_main.lua
+++ sample-lua-scripts/CSGO_main.lua
@@ -2,45 +2,99 @@
 -- This prevents any conflicts with other lua scripts
 CSGO_Example = {}
 
-local colore = clr.Corale.Colore.Core
-local keyboard = colore.Keyboard.Instance
-local thread = clr.System.Threading.Thread
+local Colore = clr.Corale.Colore
+local Razer =  Colore.Razer
+local Thread = clr.System.Threading.Thread
 
 -- CS:GO Specific fields
 
 local team = "NA"
 local _isAnimating = false
 local _phase = "NA"
+local _activity = "NA"
+local _helmet = false
 
 -- FreezeTime Animation
 CSGO_Example.FreezeTime = coroutine.create(function ()
+	Keyboard.SetAll(Colore.Core.Color.Pink)
 	while true do
 		_isAnimating = true
-		if phase ~= "freezetime" then
-		_isAnimating = false
-			coroutine.yield()			
+		if _phase == "live" then
+			
+			CSGO_Example.SetTeam(_team)
+			_isAnimating = false
+			coroutine.yield()		
 		end
-		keyboard.SetAll(colore.Color(255,0,0))
-		thread.Sleep(200)
-		keyboard.SetAll(colore.Color(0,0,255))
-		thread.Sleep(200)
+		--Keyboard.SetAll(Colore.Core.Color.Pink)
+		Keyboard.SetKey(Razer.Keyboard.Key.Escape, Colore.Core.Color.White)
+		Thread.Sleep(200)
+		--Keyboard.SetAll(Colore.Core.Color.Blue)
+		Keyboard.SetKey(Razer.Keyboard.Key.Escape, Colore.Core.Color.Black)
+		Thread.Sleep(200)
 	end
 end)
 
 
-function CSGO_Example.PhaseHandler(phase)
-
-	if phase == "freezetime" then -- Check if Phase is FreezeTime
-		DebugLua("freezetime")
+function CSGO_Example.RoundHandler(round)
+	if round["phase"] ~= _phase then
+		--DebugLua("phase changed: " .. round["phase"])
+		_phase = round["phase"]
+	end
+	
+	if _phase == "freezetime" then -- Check if Phase is FreezeTime
+		--DebugLua("phase is now freezetime")
 		coroutine.resume(CSGO_Example.FreezeTime)
+	end
+end
+
+
+function CSGO_Example.PlayerHandler(player)
+	if player["activity"] ~= _activity then
+		_activity = player["activity"]
+				
+		if _activity == "menu" then
+			Keyboard.SetAll(Colore.Core.Color.White)
+		else
+			CSGO_Example.SetTeam(player["team"])
+		end
+	end
+	DebugLua("helmet: " .. player["state"]["helmet"])
+	if player["state"]["helmet"] ~= _helmet then
+		_helmet = player["state"]["helmet"]
+		if _helmet then
+			CSGO_Example.SetTeam(_team)
+		else
+			Keyboard.SetAll(Colore.Core.Color.Red)
+		end
+	end
+
+end
+
+
+function CSGO_Example.SetTeam(team)
+	_team = team
+	if _team == "CT" then
+		Keyboard.SetAll(Colore.Core.Color.Blue)
+	else
+		Keyboard.SetAll(Colore.Core.Color(255,255,255))
 	end
 end
 
 -- our main function to handle the data 
 function CSGO_Example.handleData(json)	
 	-- Get the current phase (if any)
-	phase = json["round"]["phase"]
-	CSGO_Example.PhaseHandler(phase)
+	
+	player = json["player"]
+	DebugLua("Got player data")
+	CSGO_Example.PlayerHandler(player)
+	
+	round = json["round"]
+	CSGO_Example.RoundHandler(round)
+	
+	
+	--phase = json["round"]["phase"]
+	--CSGO_Example.PhaseHandler(phase)
+	
 end
 
 -- Finally, we must register this script in order to receive data
