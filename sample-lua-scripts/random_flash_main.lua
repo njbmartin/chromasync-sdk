1d567fd0d5cd722cc2225ea65e00ae5eb59e92ef
Nico <n.martin@movura.co.uk>
2015-12-07 16:14:36 +0000

    Update random_flash_main.lua

--- sample-lua-scripts/random_flash_main.lua
+++ sample-lua-scripts/random_flash_main.lua
@@ -5,19 +5,22 @@ function play_anim()
 	while true do
 		-- Everthing should be white (strobe effect!!!!)
 		c = Colore.Color.White
+		background = Colore.Color.Black
 		-- set keyboard colour
 		Keyboard.SetPosition(math.random(0,5), math.random(1,18), c, true)
 		-- set mousepad colour
-		
+		Mousepad.SetAll(background)
 		Mousepad[math.random(0,14)] = c
 		Mousepad[math.random(0,14)] = c
 		Mousepad[math.random(0,14)] = c
 		-- set mouse colour
+		Mouse.SetAll(background)
 		Mouse[math.random(0,17)] = c
 		Mouse[math.random(0,17)] = c
 		Mouse[math.random(0,17)] = c
 	
 		-- set keypad colour
+		Keypad.SetAll(background)
 		Keypad[math.random(0,4),math.random(0,5)] = c
 		Keypad[math.random(0,4),math.random(0,5)] = c
 		Keypad[math.random(0,4),math.random(0,5)] = c
