2c6a572b26cf6f88a97cd46b7300e18987c14b0e
Nico <n.martin@movura.co.uk>
2015-12-07 16:08:37 +0000

    Update random_flash_main.lua

--- sample-lua-scripts/random_flash_main.lua
+++ sample-lua-scripts/random_flash_main.lua
@@ -8,29 +8,20 @@ function play_anim()
 		-- set keyboard colour
 		Colore.Keyboard.Instance.SetPosition(math.random(0,5), math.random(1,18), c, true)
 		-- set mousepad colour
-		local custom = NewCustom("mousepad")
-		custom.Colors[math.random(0,14)] = c
-		custom.Colors[math.random(0,14)] = c
-		custom.Colors[math.random(0,14)] = c
-		Colore.Mousepad.Instance.SetCustom(custom)
+		
+		Mousepad[math.random(0,14)] = c
+		Mousepad[math.random(0,14)] = c
+		Mousepad[math.random(0,14)] = c
 		-- set mouse colour
-		local mouseCustom = NewCustom("mouse")		
-		mouseCustom.Colors[math.random(0,17)] = c
-		mouseCustom.Colors[math.random(0,17)] = c
-		mouseCustom.Colors[math.random(0,17)] = c
-		Colore.Mouse.Instance.SetCustom(mouseCustom)
+		Mouse[math.random(0,17)] = c
+		Mouse[math.random(0,17)] = c
+		Mouse[math.random(0,17)] = c
+	
 		-- set keypad colour
-		local keypadCustom = NewCustom("keypad")
+		Keypad[math.random(0,4),math.random(0,5)] = c
+		Keypad[math.random(0,4),math.random(0,5)] = c
+		Keypad[math.random(0,4),math.random(0,5)] = c
 
-		keypadCustom[math.random(0,4),math.random(0,5)] = c
-		keypadCustom[math.random(0,4),math.random(0,5)] = c
-		keypadCustom[math.random(0,4),math.random(0,5)] = c
-		-- WASD
-		--keypadCustom[1,2] = c
-		--keypadCustom[2,1] = c
-		--keypadCustom[2,2] = c
-		--keypadCustom[2,3] = c
-		Colore.Keypad.Instance.SetCustom(keypadCustom)
 		-- We don't want to spam the SDK, so throttle to 50ms
 		Thread.Sleep(50)
 	end
